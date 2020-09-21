f6409ba55f5438a0ea151c3b3f37b36035a24615
Francois Rigaut <francois.rigaut@etu.unice.fr>
2020-09-21 16:58:39 +0200

    Refactoring + changed routes #1
    Moved code from controller to service
    
    Ready or not is now a "ready" attribute in request body

--- service-mission/src/app.controller.ts
+++ service-mission/src/app.controller.ts
@@ -1,58 +1,31 @@
-import { Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
 import { Response } from 'express'
 import { AppService } from './app.service';
+import { PollDTO } from './poll.dto';
 
 
 @Controller()
 export class AppController {
 
-  private polling: Number = 0;
-
   constructor(private readonly appService: AppService) {}
-
-  private isReady(): Boolean {
-    return this.polling == 4;
-  }
 
   @Post('/poll/launch')
   launchPoll(@Res() res: Response): void {
-    if(this.polling === 0) {
-      this.polling = 1;
-      this.appService.sendPollToWeather();
-      res.status(HttpStatus.OK).send('Polling weather service...');
-    } else {
-      res.status(HttpStatus.FORBIDDEN).send('Poll already in progress !');
-    }
+    res.status(HttpStatus.OK).send(this.appService.launchPoll());
   }
 
-  @Post('/poll/weather/ready')
-  progressPollWeather(@Res() res: Response): void {
-    if(this.polling == 1){
-      this.polling = 2;
-      this.appService.sendPollToRocket();
-      res.status(HttpStatus.OK).send('Weather is ready!\nPolling rocket service...');
-    } else {
-      res.status(HttpStatus.FORBIDDEN).send('Poll not in progress !');
-    }
+  @Post('/poll/weather')
+  progressPollWeather(@Body() message: PollDTO, @Res() res: Response): void {
+    res.status(HttpStatus.OK).send(this.appService.progressPollWeather());
   }
 
-  @Post('/poll/rocket/ready')
+  @Post('/poll/rocket')
   progressPollRocket(@Res() res: Response): void {
-    if(this.polling == 2){
-      this.polling = 3;
-      res.status(HttpStatus.OK).send('Rocket is ready!\nPolling mission service...!');
-    } else {
-      res.status(HttpStatus.FORBIDDEN).send('Poll not in progress !');
-    }
+    res.status(HttpStatus.OK).send(this.appService.progressPollRocket());
   }
 
-  @Post('/poll/ready')
+  @Post('/poll')
   finalizePoll(@Res() res:Response): void {
-    if(this.polling == 3){
-      this.polling = 4;
-      res.status(HttpStatus.OK).send('Mission is now ready!');
-    } else {
-      res.status(HttpStatus.FORBIDDEN).send('Poll not in progress !');
-    }
+    res.status(HttpStatus.OK).send(this.appService.finalizePoll());
   }
 }
