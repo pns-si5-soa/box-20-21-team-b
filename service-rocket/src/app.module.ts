7aa0d4c5e14760c02f97eb9af174b2a63dd9f344
Richard Peres <richardperes.info@gmail.com>
2020-09-22 10:10:45 +0200

    Hotreload

--- service-rocket/src/app.module.ts
+++ service-rocket/src/app.module.ts
@@ -5,7 +5,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import {MONGO_DB, MONGO_HOST, MONGO_PORT} from "./env_variables";
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://' + MONGO_HOST+ ':' + MONGO_PORT +'/' + MONGO_DB)],
+  // imports: [MongooseModule.forRoot('mongodb://' + MONGO_HOST+ ':' + MONGO_PORT +'/' + MONGO_DB)],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
