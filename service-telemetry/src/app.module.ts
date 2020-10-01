b9370236ad5b10f61d8e6d9cb216d6fdec6b7d49
SylvainMasia <sylvain.masia@etu.unice.fr>
2020-10-01 21:47:56 +0200

    #12 RocketMetric Model and Schema

--- service-telemetry/src/app.module.ts
+++ service-telemetry/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {MONGO_DB, MONGO_HOST, MONGO_PORT} from "./env_variables";
 import {MongooseModule} from "@nestjs/mongoose";
+import { DataModule } from './data/data.module';
 
 @Module({
-  imports: [HttpModule, MongooseModule.forRoot('mongodb://' + MONGO_HOST + ':' + MONGO_PORT + '/' + MONGO_DB)],
+  imports: [HttpModule, MongooseModule.forRoot('mongodb://' + MONGO_HOST + ':' + MONGO_PORT + '/' + MONGO_DB), DataModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ service-telemetry/src/data/rocketmetric.model.ts
@@ -0,0 +1,20 @@
+import * as mongoose from 'mongoose'
+
+enum RocketMetricTopic {
+    "PROCESSUS", "POSITION"
+}
+
+export const RocketMetricSchema = new mongoose.Schema({
+    topic: { type : RocketMetricTopic, required : true},
+    description : { type : String, required: true},
+    timestamp : { type : String, required: true},
+    altitude : { type : Number, required: false }
+});
+
+export interface RocketMetric {
+    topic : string;
+    description : string;
+    timestamp : string;
+    altitude : number;
+
+}
