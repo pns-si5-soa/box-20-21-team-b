9713172e39dee1cc524bfe5dc57e2ac7f6244446
Richard Peres <richardperes.info@gmail.com>
2020-10-09 00:41:40 +0200

    Add cron + generator + fix parser

--- service-module-metrics/server.go
+++ service-module-metrics/server.go
@@ -6,8 +6,10 @@ import (
 	"github.com/gorilla/mux"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -28,7 +30,7 @@ type Metric struct {
 	Timestamp time.Time `json:"timestamp"`
 }
 
-// Custom error to return in case of a JSON parcing error
+// Custom error to return in case of a JSON parsing error
 type JSONError struct {
 	Message string `json:"Message"`
 }
@@ -36,6 +38,7 @@ type JSONError struct {
 // The current module (stateful)
 var CurrentModule Module
 
+// Max metrics entry saved in cache, currently 24h with 1 log per second
 const MaxCache int = 86400
 
 // Add a metric to the current module's cache
@@ -53,7 +56,7 @@ func appendMetric(metric Metric) {
 func getMetricsFromTimestamp(timestamp time.Time) []Metric {
 	metricsFrom := make([]Metric, 0, MaxCache)
 
-	for _, metric := range metricsFrom {
+	for _, metric := range CurrentModule.LastMetrics {
 		if metric.Timestamp.After(timestamp) {
 			metricsFrom = append(metricsFrom, metric)
 		} else {
@@ -72,48 +75,119 @@ func ok(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
-// List all the metrics after a given timestamp, or all
+// List the metrics after a given timestamp
 func metrics(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(req)
 	var listMetrics []Metric // List of metrics to return
 
 	// Get the timestamp from parameter
-	timestamp, ok := params["timestamp"]
+	timestampParam, ok := params["timestamp"]
 
 	// If there is no timestamp
 	if !ok {
-		listMetrics = CurrentModule.LastMetrics
-	} else { // There is a timestamp
-		timestamp, err := time.Parse(time.StampNano, timestamp)
-		if err != nil {
-			panic(err)
-		}
-		listMetrics = getMetricsFromTimestamp(timestamp)
+		e := JSONError{Message: "Internal Server Error"}
+		w.WriteHeader(http.StatusInternalServerError)
+		err2 := json.NewEncoder(w).Encode(e)
+		log.Panic("No timestamp provided", err2)
 	}
 
+	// TODO choose timestamp template
+	timestamp, err := time.Parse(time.RFC3339, timestampParam)
+	if err != nil {
+		log.Panic(err)
+	}
+	listMetrics = getMetricsFromTimestamp(timestamp)
+
 	// Return logs as a JSON object
-	err := json.NewEncoder(w).Encode(listMetrics)
+	jsonError := json.NewEncoder(w).Encode(listMetrics)
+	if jsonError != nil {
+		e := JSONError{Message: "Internal Server Error"}
+		w.WriteHeader(http.StatusInternalServerError)
+		err2 := json.NewEncoder(w).Encode(e)
+		log.Panic(err, err2)
+	}
+}
+
+// List all the metrics
+func allMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// Return logs as a JSON object
+	err := json.NewEncoder(w).Encode(CurrentModule.LastMetrics)
 	if err != nil {
 		e := JSONError{Message: "Internal Server Error"}
 		w.WriteHeader(http.StatusInternalServerError)
 		err2 := json.NewEncoder(w).Encode(e)
-		log.Fatal(err, err2)
+		log.Panic(err, err2)
 	}
 }
 
+// Generate & add a random metric every 1 second
+func generateMetric(done <-chan bool) {
+	ticker := time.NewTicker(1 * time.Second)
+	go func() {
+		for {
+			select {
+			case <-done:
+				ticker.Stop()
+				return
+			case <-ticker.C:
+				// Generation from last metric
+				last := CurrentModule.LastMetrics[len(CurrentModule.LastMetrics)-1]
+				appendMetric(Metric{
+					// min + rand.Float64() * (max - min)
+					// rand.Intn(max - min) + min
+					Altitude:  last.Altitude + rand.Intn(100-5) + 5,
+					Fuel:      last.Fuel + -0.1 + rand.Float32()*(-0.5 - -0.1),
+					Pressure:  last.Pressure + 0 + rand.Float32()*((7.2-last.Pressure)-0),
+					Attached:  true,
+					Speed:     last.Speed + rand.Intn(150 - -150) + -150,
+					Latitude:  last.Latitude + -0.5 + rand.Float32()*(-0.5 - -0.5),
+					Longitude: last.Longitude + -0.5 + rand.Float32()*(-0.5 - -0.5),
+					Timestamp: time.Now(),
+				})
+			}
+		}
+	}()
+}
+
 func main() {
+	// If there is a seed set in env
+	if os.Getenv("SEED") != "" {
+		seed, err := strconv.ParseInt(os.Getenv("SEED"), 10, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		rand.Seed(seed)
+	} else {
+		rand.Seed(time.Now().UnixNano())
+	}
+
 	// TODO Add backup / Restore for cache
 
 	CurrentModule = Module{LastMetrics: make([]Metric, 0, MaxCache)}
+	CurrentModule.LastMetrics = append(CurrentModule.LastMetrics, Metric{
+		Altitude:  100,
+		Fuel:      20.0,
+		Pressure:  1,
+		Attached:  true,
+		Speed:     4000,
+		Latitude:  25.333,
+		Longitude: 52.666,
+		Timestamp: time.Now(),
+	})
 
-	// TODO Add "cron" metrics generator
+	// CRON to generate random metric every second
+	gene := make(chan bool, 1)
+	generateMetric(gene)
 
 	// Create a new router to serve routes
 	router := mux.NewRouter()
 
 	// All the routes of the app
 	router.HandleFunc("/module-metrics/ok", ok).Methods("GET")
+	router.HandleFunc("/module-metrics/metrics", allMetrics).Methods("GET")
 	router.HandleFunc("/module-metrics/metrics/{timestamp}", metrics).Methods("GET")
 
 	fmt.Println("Server is running on port " + os.Getenv("PORT"))
