830266fb18a3f3a12471ac21659da2efb9657470
Richard Peres <richardperes.info@gmail.com>
2020-10-08 23:26:50 +0200

    Add documentation + timestamp handling + remove mongo & replace with "cache"

--- service-module-metrics/server.go
+++ service-module-metrics/server.go
@@ -1,90 +1,123 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"time"
-
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	//"go.mongodb.org/mongo-driver/mongo/readpref"
-	"encoding/json"
-	"github.com/gorilla/mux"
 )
 
+// A module representation
 type Module struct {
-	Altitude int     `json:"altitude"`
-	Fuel     float32 `json:"fuel"`
-	Pressure float32 `json:"pressure"`
-	Attached bool    `json:"attached"`
-	Speed    int     `json:"speed"`
-	Latitude float32 `json:"latitude"`
-	Longitude float32 `json:"longitude"`
+	LastMetrics []Metric // Cache of last 24 hours of log (1 per second)
 }
 
-//func (m Module) getMetricsAsJSON(timestamp string) (metricsJson []byte) {
-//	metricsJson, err := json.Marshal(m)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	return
-//}
+// A simple metric for telemetry
+type Metric struct {
+	Altitude  int       `json:"altitude"`
+	Fuel      float32   `json:"fuel"`
+	Pressure  float32   `json:"pressure"`
+	Attached  bool      `json:"attached"`
+	Speed     int       `json:"speed"`
+	Latitude  float32   `json:"latitude"`
+	Longitude float32   `json:"longitude"`
+	Timestamp time.Time `json:"timestamp"`
+}
 
+// Custom error to return in case of a JSON parcing error
+type JSONError struct {
+	Message string `json:"Message"`
+}
+
+// The current module (stateful)
+var CurrentModule Module
+
+const MaxCache int = 86400
+
+// Add a metric to the current module's cache
+func appendMetric(metric Metric) {
+	// If the cache is full (24 hours of logs with 1 log / second), pop the last entry (FIFO)
+	if len(CurrentModule.LastMetrics) == MaxCache {
+		CurrentModule.LastMetrics = CurrentModule.LastMetrics[:len(CurrentModule.LastMetrics)-1]
+	}
+
+	// Add the current metric at the first place (FIFO)
+	CurrentModule.LastMetrics = append([]Metric{metric}, CurrentModule.LastMetrics...)
+}
+
+// Return all the metrics newer than the timestamp
+func getMetricsFromTimestamp(timestamp time.Time) []Metric {
+	metricsFrom := make([]Metric, 0, MaxCache)
+
+	for _, metric := range metricsFrom {
+		if metric.Timestamp.After(timestamp) {
+			metricsFrom = append(metricsFrom, metric)
+		} else {
+			return metricsFrom
+		}
+	}
+
+	return metricsFrom
+}
+
+// Basic OK route for healthcheck
 func ok(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "ok")
+	_, err := io.WriteString(w, "ok")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
+// List all the metrics after a given timestamp, or all
 func metrics(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(req)
+	var listMetrics []Metric // List of metrics to return
 
+	// Get the timestamp from parameter
 	timestamp, ok := params["timestamp"]
+
+	// If there is no timestamp
 	if !ok {
-		log.Print("No timestamp arg (/metrics)")
+		listMetrics = CurrentModule.LastMetrics
+	} else { // There is a timestamp
+		timestamp, err := time.Parse(time.StampNano, timestamp)
+		if err != nil {
+			panic(err)
+		}
+		listMetrics = getMetricsFromTimestamp(timestamp)
 	}
 
-	if timestamp == "1" {
-		fmt.Println("Got arg !")
-	}
-
-	module := Module{
-		Altitude:  100,
-		Fuel:      5.3,
-		Pressure:  0.9,
-		Attached:  true,
-		Speed:     130,
-		Latitude:  45.766,
-		Longitude: 8.335,
-	}
-	err := json.NewEncoder(w).Encode(module)
+	// Return logs as a JSON object
+	err := json.NewEncoder(w).Encode(listMetrics)
 	if err != nil {
-		log.Fatal(err)
+		e := JSONError{Message: "Internal Server Error"}
+		w.WriteHeader(http.StatusInternalServerError)
+		err2 := json.NewEncoder(w).Encode(e)
+		log.Fatal(err, err2)
 	}
-}
-
-func mongoInit() context.CancelFunc {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://" + os.Getenv("MONGO_HOST") + ":" + os.Getenv("MONGO_PORT") + "/" + os.Getenv("MONGO_DB")))
-	if err != nil {
-		log.Fatal(err)
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return cancel
 }
 
 func main() {
+	// TODO Add backup / Restore for cache
+
+	CurrentModule = Module{LastMetrics: make([]Metric, 0, MaxCache)}
+
+	// TODO Add "cron" metrics generator
+
+	// Create a new router to serve routes
 	router := mux.NewRouter()
+
+	// All the routes of the app
 	router.HandleFunc("/module-metrics/ok", ok).Methods("GET")
 	router.HandleFunc("/module-metrics/metrics/{timestamp}", metrics).Methods("GET")
 
 	fmt.Println("Server is running on port " + os.Getenv("PORT"))
-	log.Fatal(http.ListenAndServe(":" + os.Getenv("PORT"), router))
+
+	// Start the server
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
 }
