b98fa7437696edde965fe00371940ed5c4030491
Richard Peres <richardperes.info@gmail.com>
2020-10-08 19:57:22 +0200

    Add /ok route for healthcheck in service metrics

--- service-module-metrics/src/app.controller.ts
+++ service-module-metrics/src/app.controller.ts
@@ -1,10 +1,9 @@
 import { Controller } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller('telemetry')
 export class AppController {
 
-  constructor(private readonly appService: AppService) {
-
+  ok(): string {
+    return 'ok';
   }
 }
--- service-module-metrics/src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import {Injectable, Logger} from '@nestjs/common';
-
-
-@Injectable()
-export class AppService {
-  constructor() {
-
-  }
-}
