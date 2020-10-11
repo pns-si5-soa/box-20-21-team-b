7727a65ef3ff33062a5fb189b776eed26a3074d7
SylvainMasia <sylvain.masia@etu.unice.fr>
2020-10-11 11:30:37 +0200

    Refactor service telemetry

--- service-telemetry/src/app.controller.ts
+++ service-telemetry/src/app.controller.ts
@@ -1,36 +1,9 @@
-import {Controller, Get, Param} from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('telemetry')
+@Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
-  /**
-   * @param params
-   * beginDate: yyyy-mm-ddThh:ii
-   * endDate: yyyy-mm-ddThh:ii
-   * example -> http://localhost:3004/rocket-metrics/2020-10-07T11:35/2020-10-07T11:36
-   */
-  @Get('/rocket-metrics/:beginDate/:endDate')
-  async getRocketMetrics(@Param() params) {
-    let beginDate = params.beginDate;
-    if(beginDate === null || beginDate === 'null') {
-      beginDate = new Date();
-      beginDate.setMinutes(beginDate.getMinutes() - 3); //if no hour given, get current time and remove trhee minutes
-      beginDate = beginDate.getTime();
-    }else{
-      beginDate = Date.parse(beginDate + '+02:00');
-    }
-    let endDate = params.endDate;
-    if(endDate === null || endDate === 'null') {
-      endDate = new Date();
-      endDate.setMinutes(endDate.getMinutes() + 1); //if no hour given, get current time and add one minute
-      endDate = endDate.getTime();
-    }else{
-      endDate = Date.parse(endDate + '+02:00');
-    }
-    return await this.appService.retrieveRocketMetrics(beginDate, endDate);
-  }
 }
 
 
--- service-telemetry/src/app.module.ts
+++ service-telemetry/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import {MONGO_DB, MONGO_HOST, MONGO_PORT} from "./env_variables";
 import {MongooseModule} from "@nestjs/mongoose";
 import {RocketMetricSchema} from "./model/rocketmetric.model";
+import { DataModule } from './data/data.module';
 
 @Module({
-  imports: [HttpModule, MongooseModule.forRoot('mongodb://' + MONGO_HOST + ':' + MONGO_PORT + '/' + MONGO_DB), MongooseModule.forFeature([{name: 'RocketMetric', schema: RocketMetricSchema}])],
+  imports: [HttpModule, MongooseModule.forRoot('mongodb://' + MONGO_HOST + ':' + MONGO_PORT + '/' + MONGO_DB), MongooseModule.forFeature([{name: 'RocketMetric', schema: RocketMetricSchema}]), DataModule],
   controllers: [AppController],
   providers: [AppService],
 })
