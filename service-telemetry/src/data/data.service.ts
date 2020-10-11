2bc1ca2f3bd14138532e2ccd7801747f7711e336
SylvainMasia <sylvain.masia@etu.unice.fr>
2020-10-11 13:53:28 +0200

    Telemetry prepared for metrics #17 #18

--- service-telemetry/src/data/data.service.ts
+++ service-telemetry/src/data/data.service.ts
@@ -2,6 +2,7 @@ import { HttpService, Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { Model } from 'mongoose'
+import { BOOSTER_HOST, BOOSTER_PORT, PROBE_HOST, PROBE_PORT, STAGE_HOST, STAGE_PORT } from 'src/env_variables';
 import { RocketMetric } from '../model/rocketmetric.model';
 
 @Injectable()
@@ -12,8 +13,17 @@ export class DataService {
 
   //Task done every 10 seconds
   @Cron(CronExpression.EVERY_10_SECONDS, { name: 'metrics' })
-  getMetrics() {
+  async getMetrics() {
     Logger.log("Metrics log");
+    //Todo remove comments when services are ready
+    /*let metricProbe = new this.rocketMetric(this.httpService.get(`${PROBE_HOST}:${PROBE_PORT}/metrics`));
+    await metricProbe.save();
+
+    let metricStage = new this.rocketMetric(this.httpService.get(`${STAGE_HOST}:${STAGE_PORT}/metrics`));
+    await metricStage.save();
+
+    let metricBooster = new this.rocketMetric(this.httpService.get(`${BOOSTER_HOST}:${BOOSTER_PORT}/metrics`));
+    await metricBooster.save();*/
   }
 
   async retrieveRocketMetrics(beginDate: number, endDate: number): Promise<RocketMetric[]> {
