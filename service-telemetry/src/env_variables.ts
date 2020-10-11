6c32c92589fbaf63742cc8520ee66402ac74a3eb
SylvainMasia <sylvain.masia@etu.unice.fr>
2020-10-11 11:39:50 +0200

    Env variable for golang rocket modules setup wip

--- service-telemetry/src/env_variables.ts
+++ service-telemetry/src/env_variables.ts
@@ -2,4 +2,13 @@ export const PORT = process.env.PORT ? process.env.PORT : 3004;
 
 export const MONGO_HOST = process.env.MONGO_HOST ? process.env.MONGO_HOST : "localhost"
 export const MONGO_PORT = process.env.MONGO_PORT ? process.env.MONGO_PORT : 27017
+
+
+export const PROBE_HOST = process.env.PROBE_HOST ? process.env.PROBE_HOST : "localhost"
+export const PROBE_PORT = process.env.PROBE_PORT ? process.env.PROBE_PORT : 3003
+export const STAGE_HOST = process.env.STAGE_HOST ? process.env.STAGE_HOST : "localhost"
+export const STAGE_PORT = process.env.STAGE_PORT ? process.env.STAGE_PORT : 3003
+export const BOOSTER_HOST = process.env.BOOSTER_HOST ? process.env.BOOSTER_HOST : "localhost"
+export const BOOSTER_PORT = process.env.BOOSTER_PORT ? process.env.BOOSTER_PORT : 3003
+
 export const MONGO_DB = process.env.MONGO_DB ? process.env.MONGO_DB : "nest"
