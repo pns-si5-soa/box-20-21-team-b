57c321c5a1d6c06e947abdaa1961a2ad657f72ae
Richard Peres <richardperes.info@gmail.com>
2020-10-18 17:48:28 +0200

    Update path & ports + fix grafana dashboard default

--- service-rocket/src/actions.stub.ts
+++ service-rocket/src/actions.stub.ts
@@ -1,18 +1,18 @@
 import {ModuleActionsClient} from "../rpc/actions_grpc_pb";
 import {credentials} from "grpc";
-import {PROBE_HOST, PROBE_PORT, BOOSTER_HOST, BOOSTER_PORT, STAGE_HOST, STAGE_PORT} from "./env_variables";
+import {MODULES_HOST, MODULES_PORT} from "./env_variables";
 
 export const clientBooster = new ModuleActionsClient(
-    BOOSTER_HOST+':'+BOOSTER_PORT,
+    MODULES_HOST+':'+MODULES_PORT,
     credentials.createInsecure()
 );
 
 export const clientProbe = new ModuleActionsClient(
-    PROBE_HOST+':'+PROBE_PORT,
+    MODULES_HOST+':'+MODULES_PORT,
     credentials.createInsecure()
 );
 
 export const clientStage = new ModuleActionsClient(
-    STAGE_HOST+':'+STAGE_PORT,
+    MODULES_HOST+':'+MODULES_PORT,
     credentials.createInsecure()
 );
