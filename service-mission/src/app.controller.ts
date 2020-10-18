a285760d73f08d79be23ebf834a4587becb0d1dc
FrancoisRigaut <francois.rigaut@etu.unice.fr>
2020-10-18 16:56:45 +0200

    Conflict resolved

--- service-mission/src/app.controller.ts
+++ service-mission/src/app.controller.ts
@@ -1,31 +1,9 @@
-<<<<<<< HEAD
 import {Controller, Get} from "@nestjs/common";
 
 @Controller('mission')
-=======
-import { Controller, Get } from '@nestjs/common';
-
-<<<<<<<< HEAD:service-mission/src/app.controller.ts
-@Controller("/mission")
 export class AppController {
     @Get('/ok')
     ok(): string {
         return "ok";
-========
-@Controller('/telemetry')
->>>>>>> prometheus
-export class AppController {
-
-    @Get('/ok')
-    healthCheck(): string {
-        return 'ok';
-<<<<<<< HEAD
     }
 }
-=======
->>>>>>>> prometheus:service-telemetry/src/app.controller.ts
-    }
-}
-
-
->>>>>>> prometheus
