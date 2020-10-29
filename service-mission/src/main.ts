509b570e762a1ecc5bc786b51d148ddcef57cc8f
PERES-Richard <richard.peres@orange.com>
2020-10-29 17:28:33 +0100

    Add & Fix ingress + add mongo (wip

--- service-mission/src/main.ts
+++ service-mission/src/main.ts
@@ -1,6 +1,6 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
-import {PORT} from './env_variables';
+import {MONGO_DB, MONGO_HOST, MONGO_PORT, PORT} from './env_variables';
 import {FastifyAdapter, NestFastifyApplication,} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
@@ -9,6 +9,7 @@ async function bootstrap() {
         new FastifyAdapter({logger: true})
     );
     await app.listen(Number(PORT), '0.0.0.0');
+    console.log("mongo URL : " + MONGO_HOST + ":" + MONGO_PORT + "/" + MONGO_DB)
 }
 
 bootstrap();
