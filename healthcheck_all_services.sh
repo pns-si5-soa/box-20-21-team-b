e28a40848c34fe9772e913c9abacb8000bee74f8
PERES-Richard <43060465+PERES-Richard@users.noreply.github.com>
2020-10-30 14:56:45 +0100

    fix typo

--- healthcheck_all_services.sh
+++ healthcheck_all_services.sh
@@ -10,4 +10,4 @@ echo Test Weather service..
 ./healthcheck_service.sh http://localhost/weather/ok
 
 echo Test Webcaster service..
-./healthcheck_service.sh http://localhost/Webcaster/ok
+./healthcheck_service.sh http://localhost/webcaster/ok
